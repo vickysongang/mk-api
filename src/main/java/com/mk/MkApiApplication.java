21e3dbfb1c42c980a330ba889372599c9ad957bf
angsong <angsong@wizteco.com>
2019-09-05 21:00:38 +0800

    initinal submit

---
+++ src/main/java/com/mk/MkApiApplication.java
@@ -0,0 +1,13 @@
+package com.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkApiApplication.class, args);
+	}
+
+}
